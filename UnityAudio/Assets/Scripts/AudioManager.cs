6022e01c76c48faadbc63778237b694258cc8bda
MUBASSHIRAHMED\Mubasshir Ahmed <MubasshirAhmed26@gmail.com>
2018-08-15 13:07:36 +0600

    first commit

---
+++ UnityAudio/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] audioClip;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnBtnClick(int audioNumber)
+    {
+        audioSource.clip = audioClip[audioNumber];
+        audioSource.Play();
+    }
+}
